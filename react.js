0948c63eb69b2dd3661725dca217045764d62c97
Donghwan Kim <flowersinthesand@gmail.com>
2014-04-07 16:52:31 +0900

    Remove idGenerator option #9

--- react.js
+++ react.js
@@ -351,7 +351,7 @@
         // Saves original URL
         opts.url = url;
         // Generates socket id,
-        opts.id = opts.idGenerator.call(self);
+        opts.id = support.uuid();
         opts.crossDomain = !!(parts &&
             // protocol and hostname
             (parts[1] != location.protocol || parts[2] != location.hostname ||
@@ -899,6 +899,14 @@
                     }
                 })("", {"": value});
         },
+        uuid: function() {
+            // Generates a random UUID
+            // Logic borrowed from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+            return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c === "x" ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
         storage: !!(window.localStorage && window.StorageEvent)
     };
     guid = support.now();
@@ -960,14 +968,6 @@
         sharing: false,
         reconnect: function(lastDelay) {
             return 2 * (lastDelay || 250);
-        },
-        idGenerator: function() {
-            // Generates a random UUID
-            // Logic borrowed from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
-            return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-                var r = Math.random() * 16 | 0, v = c === "x" ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
         }
         // See the fifth at http://blogs.msdn.com/b/ieinternals/archive/2010/05/13/xdomainrequest-restrictions-limitations-and-workarounds.aspx
         // and http://stackoverflow.com/questions/6453779/maintaining-session-by-rewriting-url
