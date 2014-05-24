3a188743d7924a35bb9ce16dfd4efb2eb041d970
Donghwan Kim <flowersinthesand@gmail.com>
2014-05-24 23:16:05 +0900

    Fixes #19 - Update copyright owner

--- vibe.js
+++ vibe.js
@@ -2,7 +2,7 @@
  * Vibe v3.0.0-Alpha1
  * http://atmosphere.github.io/vibe/
  * 
- * Copyright 2011-2014, Donghwan Kim 
+ * Copyright 2014 The Vibe Project 
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
